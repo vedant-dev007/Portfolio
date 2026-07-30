da0a718948fed44edc5e98e8b4160622ad023138
vedant-dev007 <vedantgithub007@gmail.com>
2026-07-30 19:47:38 +0530

    Deploy Next.js static export to GitHub Pages

---
+++ _next/static/chunks/1oem-e76l5_ua.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,31602,t=>{t.v(o=>Promise.all(["static/chunks/3p6hs6xta0-va.js"].map(o=>t.l(o))).then(()=>o(12094)))}]);
